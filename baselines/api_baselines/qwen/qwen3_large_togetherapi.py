1232b8031bd227d9c03c638ad242cbea694c0a36
Li Hao <49901804+HowardLi1984@users.noreply.github.com>
2025-06-11 22:30:46 +0800

    Update qwen3_large_togetherapi.py

--- baselines/api_baselines/qwen/qwen3_large_togetherapi.py
+++ baselines/api_baselines/qwen/qwen3_large_togetherapi.py
@@ -1,6 +1,6 @@
 from together import Together
 base_url="https://api.together.xyz/v1"
-api_key="tgp_v1_-rSzLz-5TJa_vfnsKubFDsWI8ApOIOeRlir2OXDm-RU"
+api_key="your-key"
 
 client = Together(api_key=api_key, base_url=base_url)
 response = client.chat.completions.create(
